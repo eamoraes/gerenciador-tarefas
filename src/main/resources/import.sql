0f098b03554a98b99ef1b2f2b197a6b96ebaae0f
eamoraes <eamoraes@gmail.com>
2019-07-19 22:00:21 -0300

    removendo import automatico

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 --senha = 1
-insert into usr_usuarios(USR_ID, USR_EMAIL, USR_SENHA) values (1, 'eamoraes@gmail.com', '$2a$10$ekNbDxM.aoJ1a.5AxR.6leIamzSMpfasSz.Zmr96F93k2t/eT4nrC');
-insert into TAR_TAREFAS (TAR_ID ,TAR_CONCLUIDA ,TAR_DATA_EXPIRACAO ,TAR_DESCRICAO ,TAR_TITULO, USR_ID) values (1, false,'2012-1-1','Teste Descricao','Teste Titulo', 1);
+--insert into usr_usuarios(USR_ID, USR_EMAIL, USR_SENHA) values (1, 'eamoraes@gmail.com', '$2a$10$ekNbDxM.aoJ1a.5AxR.6leIamzSMpfasSz.Zmr96F93k2t/eT4nrC');
+--insert into TAR_TAREFAS (TAR_ID ,TAR_CONCLUIDA ,TAR_DATA_EXPIRACAO ,TAR_DESCRICAO ,TAR_TITULO, USR_ID) values (1, false,'2012-1-1','Teste Descricao','Teste Titulo', 1);
