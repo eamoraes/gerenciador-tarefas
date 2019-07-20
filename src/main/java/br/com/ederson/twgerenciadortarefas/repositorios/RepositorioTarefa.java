15331dd56423d6a95e29b876fde7d6e9bcefacf6
eamoraes <eamoraes@gmail.com>
2019-07-19 21:54:41 -0300

    first commit

---
+++ src/main/java/br/com/ederson/twgerenciadortarefas/repositorios/RepositorioTarefa.java
@@ -0,0 +1,18 @@
+package br.com.ederson.twgerenciadortarefas.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.ederson.twgerenciadortarefas.modelos.Tarefa;
+
+public interface RepositorioTarefa extends JpaRepository<Tarefa, Long>{
+	
+	@Query("SELECT t FROM Tarefa t WHERE t.usuario.email = :emailUsuario")
+	List<Tarefa> carregarTarefasPorUsuario(@Param("emailUsuario") String email);
+	
+	List<Tarefa> findByUsuarioEmail(String email);
+
+}
